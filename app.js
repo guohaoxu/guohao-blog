adcc462e997d49b0a6d6cb24c1856d2231cd1186
guohao <guohao0817@gmail.com>
2016-02-21 20:21:11 +0800

    first push origin

--- app.js
+++ app.js
@@ -1,59 +1,68 @@
-var path = require('path');
+var express = require('express'),
+    app = express(),
+	path = require('path'),
+	bodyParser = require('body-parser'),
+	cookieParser = require('cookie-parser'),
+	session = require('express-session'),
+	flash = require('connect-flash'),
+	
+	favicon = require('serve-favicon'),
+	logger = require('morgan'),
+	
+	routes = require('./routes/index'),
+	settings = require('./settings');
 
-var express = require('express');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
-var flash = require('connect-flash');
-var multer  = require('multer');
+app.set('port', process.env.PORT || 3001);
 
-var routes = require('./routes/index');
-var settings = require('./settings');
-
-var fs = require('fs');
-var accessLog = fs.createWriteStream('access.log', {flags: 'a'});
-var errorLog = fs.createWriteStream('error.log', {flags: 'a'});
-
-var app = express();
-
-app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
-app.use(logger('dev'));
-app.use(logger({stream: accessLog}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({
-  dest: './public/images',
-  rename: function (fieldname, filename) {
-    return filename;
-  }
-}));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session({
-  secret: settings.cookieSecret,
-  key: settings.db,//cookie name
-  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
-  store: new MongoStore({
-    db: settings.db,
-    host: settings.host,
-    port: settings.port
-  })
+	secret: settings.cookieSecret,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 *30 },
+    resave: false,
+    saveUninitialized: true
 }));
+
+app.use(logger('dev'));
+
 app.use(flash());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 routes(app);
 
-app.use(function (err, req, res, next) {
-  var meta = '[' + new Date() + '] ' + req.url + '\n';
-  errorLog.write(meta + err.stack + '\n');
-  next();
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+if(app.get('env') === 'development') {
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
+});
+
+
+app.listen(app.get('port'), function () {
+    console.log('Server listening on ', app.get('port'));
 });
--- settings.js
+++ settings.js
@@ -1,6 +1,4 @@
-module.exports = { 
-  cookieSecret: 'myblog', 
-  db: 'blog', 
-  host: 'localhost',
-  port: 27017
-};
+module.exports = {
+	cookieSecret: 'guohao-blog',
+    url: 'mongodb://localhost:27017/guohao-blog'
+}
