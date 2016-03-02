207bd446dff5c8133efa1a825b07c01f5854f0fa
guohao <guohao0817@gmail.com>
2016-03-02 20:03:23 +0800

    indent

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,21 @@
 module.export = function (grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd) %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-	    dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd) %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
 
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['uglify']);
-  
+    grunt.registerTask('default', ['uglify']);
 }
--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require('express'),
-  app = express(),
-  path = require('path'),
-  bodyParser = require('body-parser'),
-  cookieParser = require('cookie-parser'),
-  session = require('express-session'),
-  flash = require('connect-flash'),
+app = express(),
+path = require('path'),
+bodyParser = require('body-parser'),
+cookieParser = require('cookie-parser'),
+session = require('express-session'),
+flash = require('connect-flash'),
 
-  favicon = require('serve-favicon'),
-  logger = require('morgan'),
+favicon = require('serve-favicon'),
+logger = require('morgan'),
 
-  routes = require('./routes/index'),
-  settings = require('./settings');
+routes = require('./routes/index'),
+settings = require('./settings');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -20,17 +20,17 @@ app.set('view engine', 'ejs');
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session({
-  secret: settings.cookieSecret,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 30
-  },
-  resave: false,
-  saveUninitialized: true
+    secret: settings.cookieSecret,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 30
+    },
+    resave: false,
+    saveUninitialized: true
 }));
 
 app.use(logger('dev'));
@@ -43,31 +43,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 routes(app);
 
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  res.render('404');
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    res.render('404');
+    next(err);
 });
 
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
 app.listen(app.get('port'), function() {
-  console.log('Server listening on ', app.get('port'));
+    console.log('Server listening on ', app.get('port'));
 });
