8c0fd3adaf1c36bcce85ff6e62ecc639915b03b4
guohao <guohao0817@gmail.com>
2016-03-16 02:48:53 +0800

    xiugai setting.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
 	cookieSecret: 'guohao-blog',
     //url: 'mongodb://localhost:27017/guohao-blog'
-    url: 'mongodb://guohaoxu:guohao2016@mlab@ds015929.mlab.com:15929/guohao-blog'
+    url: 'mongodb://guohaoxu:guohao2016@ds015929.mlab.com:15929/guohao-blog'
 }
